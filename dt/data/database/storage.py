711df680b35dd1760bcaea35f26b84aeaa62e5c1
JustRayCB <rayancbpro@gmail.com>
2025-03-02 18:58:18 +0100

    (draft) Database class and db init file (not tested)

---
+++ dt/data/database/storage.py
@@ -0,0 +1,119 @@
+import sqlite3
+
+from dt.sensors.kinds.base_sensor import Sensor
+
+
+class Storage:
+    def __init__(self) -> None:
+        self.db_path = "plant_dt.db"
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self) -> None:
+        """Create the table to store the data"""
+        # I already have a db_init.sql file, so I will use it to create the table
+        with open("db_init.sql", "r") as f:
+            self.cursor.executescript(f.read())
+            self.conn.commit()
+
+    def insert_data(self, data: dict[str, any]) -> None:
+        """Insert read data into the database
+
+        Parameters
+        ----------
+        data : dict[str, any]
+            Data read from a (one) sensor with their metadata
+
+        """
+        self.cursor.execute(
+            "INSERT INTO sensors_data (sensor_id, value, unit, timestamp) VALUES (?, ?, ?, ?)",
+            (data["sensor_id"], data["value"], data["unit"], data["timestamp"]),
+        )
+        self.conn.commit()
+
+    def insert_datas(self, datas: dict[str, dict[str, any]]) -> None:
+        """Insert read data into the database
+
+        Parameters
+        ----------
+        datas : dict[str, dict[str, any]]
+            Data read from the (multiple) sensors with their metadata
+
+        """
+        for data in datas.values():
+            self.insert_data(data)
+
+    def get_data(self, sensor_id: int, limit: int = 10) -> list[dict[str, any]]:
+        """Get the last data read from a sensor
+
+        Parameters
+        ----------
+        sensor_id : int
+            The id of the sensor
+        limit : int, optional
+            The number of data to retrieve, by default 10
+
+        Returns
+        -------
+        list[dict[str, any]]
+            The last data read from the sensor
+
+        """
+        self.cursor.execute(
+            "SELECT * FROM sensors_data WHERE sensor_id = ? ORDER BY timestamp DESC LIMIT ?",
+            (sensor_id, limit),
+        )
+        return self.cursor.fetchall()
+
+    def get_sensor_id(self, sensor_name: str) -> int:
+        """Get the id of a sensor
+
+        Parameters
+        ----------
+        sensor_name : str
+            The name of the sensor
+
+        Returns
+        -------
+        int
+            The id of the sensor
+
+        """
+        self.cursor.execute(
+            "SELECT id FROM sensors WHERE name = ?",
+            (sensor_name,),
+        )
+        return self.cursor.fetchone()
+
+    def add_sensor(self, sensor: Sensor) -> None:
+        """Add a sensor to the database
+
+        Parameters
+        ----------
+        sensor : Sensor
+            The sensor to add
+
+        """
+        self.cursor.execute(
+            "INSERT INTO sensors (name) VALUES (?)",
+            (sensor.name,),
+        )
+        self.conn.commit()
+        sensor.id = self.cursor.lastrowid  # pyright: ignore[]
+        assert sensor.id > 0, "Sensor ID not set"
+
+    def bind_sensors(self, sensor: Sensor) -> None:
+        """Bind the sensor to the database
+
+        Parameters
+        ----------
+        sensor : Sensor
+            The sensor to bind
+
+        """
+        temp_id = self.get_sensor_id(sensor.name)
+        if temp_id:
+            sensor.id = temp_id
+        else:
+            self.add_sensor(sensor)
