6f24e74d33edf844731f02793005ce929774706b
JustRayCB <rayancbpro@gmail.com>
2025-03-11 23:15:03 +0100

    (code) Move the main file at the same level of the dt folder

---
+++ main.py
@@ -0,0 +1,28 @@
+from dt.sensors import MockMoistureSensor, Sensor, SensorManager
+from dt.utils import get_logger
+
+
+def main():
+    logger = get_logger(__name__)
+    logger.info("Starting main")
+
+    sensor_manager = SensorManager()
+    moisture_sensor = MockMoistureSensor("moisture_sensor", 5, 100)
+    sensor_manager.add_sensor(moisture_sensor)
+
+    try:
+        while True:
+            sensor_manager.read_all_sensors()
+            print("Reading all sensors")
+    except KeyboardInterrupt:
+        logger.info("Exiting main")
+    finally:
+        logger.info("Cleaning up")
+        del sensor_manager
+        logger.info("Exiting")
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
