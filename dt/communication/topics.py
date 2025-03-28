87a9b02b942e11b4fc6663232d1df0f64b714013
JustRayCB <rayancbpro@gmail.com>
2025-03-28 22:59:00 +0100

    (code) Add some misc methods to MQTTTopics

--- dt/communication/topics.py
+++ dt/communication/topics.py
@@ -11,3 +11,13 @@ class MQTTTopics(StrEnum):
     SOIL_MOISTURE = _PREFIX_SENSOR + "soil_moisture"
     LIGHT_INTENSITY = _PREFIX_SENSOR + "light_intensity"
     CAMERA_IMAGE = _PREFIX_SENSOR + "camera_image"
+
+    @classmethod
+    def from_short_name(cls, short_name: str) -> "MQTTTopics":
+        """Get the MQTT topic from the short name"""
+        return cls[short_name.upper()]
+
+    @property
+    def short_name(self) -> str:
+        """Get the short name of the topic"""
+        return self.name.split("/")[-1]
