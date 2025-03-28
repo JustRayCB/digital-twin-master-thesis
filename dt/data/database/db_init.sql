6448353fdb2ed87ca33e3592965d288f093be40a
JustRayCB <rayancbpro@gmail.com>
2025-03-28 22:59:57 +0100

    (code) Add attributes to sensors table + add new data_types table

--- dt/data/database/db_init.sql
+++ dt/data/database/db_init.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS sensors (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) UNIQUE NOT NULL
+    name VARCHAR(255) UNIQUE NOT NULL,
+    pin INTEGER NOT NULL,
+    read_interval INTEGER NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS sensors_data (
@@ -9,5 +11,15 @@ CREATE TABLE IF NOT EXISTS sensors_data (
     value REAL NOT NULL,
     unit VARCHAR(255) NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_type VARCHAR(255) NOT NULL,
     FOREIGN KEY(sensor_id) REFERENCES sensors(id)
+    FOREIGN KEY(data_type) REFERENCES sensors_data_type(name)
 );
+
+CREATE TABLE IF NOT EXISTS sensors_data_type (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+INSERT OR IGNORE INTO sensors_data_type (name) VALUES 
+    ("temperature"), ("soil_moisture"), ("humidity"), ("light_intensity"), ("camera_image")
