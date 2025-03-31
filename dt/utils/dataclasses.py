3c209ccca6a9137be891078e6e0a9d02e98e2984
JustRayCB <rayancbpro@gmail.com>
2025-03-31 23:10:56 +0200

    (code) Adding a dataclass for the Timestamped query of the DB + some validation methods

--- dt/utils/dataclasses.py
+++ dt/utils/dataclasses.py
@@ -37,7 +37,6 @@ class SensorData:
         return json.dumps(self.__dict__)
 
     @classmethod
-    @classmethod
     def from_json(cls, json_data: Union[str, dict]):
         if isinstance(json_data, str):
             data = json.loads(json_data)
@@ -60,6 +59,23 @@ class SensorData:
         Returns the last part of the MQTT topic (sensor's data)
         """
         return self.topic.short_name
+
+    @staticmethod
+    def validate_json(json_data: str) -> bool:
+        try:
+            data = json.loads(json_data)
+            assert all(
+                key in data for key in SensorData.__annotations__.keys()
+            ), "Missing keys in JSON data"
+            assert isinstance(data["sensor_id"], int), "sensor_id must be an int"
+            assert isinstance(data["timestamp"], (int, float)), "timestamp must be a float"
+            assert isinstance(data["value"], (int, float)), "value must be a float"
+            assert isinstance(data["unit"], str), "unit must be a str"
+            assert isinstance(data["topic"], str), "topic must be a str"
+            # assert data["topic"] in MQTTTopics.__members__, "topic must be a valid MQTT topic"
+            return True
+        except Exception:
+            return False
 
 
 @dataclass
@@ -105,6 +121,66 @@ class SensorDataClass:
     def validate_json(json_data: str) -> bool:
         try:
             data = json.loads(json_data)
-            return all(key in data for key in SensorDataClass.__annotations__.keys())
+            assert all(
+                key in data for key in SensorDataClass.__annotations__.keys()
+            ), "Missing keys in JSON data"
+            assert isinstance(data["sensor_id"], int), "sensor_id must be an int"
+            assert isinstance(data["name"], str), "name must be a str"
+            assert isinstance(data["pin"], int), "pin must be an int"
+            assert isinstance(data["read_interval"], int), "read_interval must be an int"
+            return True
+        except Exception:
+            return False
+
+
+@dataclass
+class DBTimestampQuery:
+    """Represents a query to the database to get the data from a specific timestamp.
+
+    Attributes
+    ----------
+    from_timestamp : The start timestamp of the query.
+    to_timestamp : The end timestamp of the query.
+    data_type : The type of data to query.
+    """
+
+    from_timestamp: float
+    to_timestamp: float
+    data_type: str
+
+    def __post_init__(self):
+        self.from_timestamp = float(self.from_timestamp)
+        self.to_timestamp = float(self.to_timestamp)
+        self.data_type = str(self.data_type)
+
+    def to_json(self) -> str:
+        return json.dumps(self.__dict__)
+
+    @classmethod
+    def from_json(cls, json_data: Union[str, dict]):
+        if isinstance(json_data, str):
+            data = json.loads(json_data)
+        elif isinstance(json_data, dict):
+            data = json_data
+        else:
+            raise TypeError("json_data must be a str or dict")
+        return cls(**data)
+
+    @staticmethod
+    def validate_json(json_data: str) -> bool:
+        try:
+            data = json.loads(json_data)
+            assert all(
+                key in data for key in DBTimestampQuery.__annotations__.keys()
+            ), "Missing keys in JSON data"
+            assert isinstance(
+                data["from_timestamp"], (int, float)
+            ), "from_timestamp must be a float"
+            assert isinstance(data["to_timestamp"], (int, float)), "to_timestamp must be a float"
+            assert isinstance(data["data_type"], str), "data_type must be a str"
+            assert data["data_type"] in [
+                t.short_name for t in MQTTTopics if t != MQTTTopics._PREFIX_SENSOR
+            ], f"Topic {data['topic']} is not a valid MQTT topic"
+            return True
         except Exception:
             return False
