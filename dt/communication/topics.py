1c0c65e9c65ae106b08ef3e90de2606f57ddd47b
JustRayCB <rayancbpro@gmail.com>
2025-04-08 16:06:25 +0200

    (config) Replace "/" by "." as "/" is illegal in kafka

--- dt/communication/topics.py
+++ dt/communication/topics.py
@@ -4,7 +4,7 @@ from enum import StrEnum
 class Topics(StrEnum):
     """MQTT topics for communication between the modules"""
 
-    _PREFIX_SENSOR = "dt/sensors/"
+    _PREFIX_SENSOR = "dt.sensors."
     SENSORS_DATA = _PREFIX_SENSOR + "data"
     TEMPERATURE = _PREFIX_SENSOR + "temperature"
     HUMIDITY = _PREFIX_SENSOR + "humidity"
@@ -20,16 +20,16 @@ class Topics(StrEnum):
     @property
     def raw(self) -> str:
         """Raw topic name"""
-        split = self.value.split("/")
-        raw_name = f"{'/'.join(split[:-1])}/raw"
-        return f"{raw_name}/{self.short_name}"
+        split = self.value.split(".")
+        raw_name = f"{'.'.join(split[:-1])}.raw"
+        return f"{raw_name}.{self.short_name}"
 
     @property
     def processed(self) -> str:
         """Processed topic name"""
-        split = self.value.split("/")
-        processed_name = f"{'/'.join(split[:-1])}/processed"
-        return f"{processed_name}/{self.short_name}"
+        split = self.value.split(".")
+        processed_name = f"{'.'.join(split[:-1])}.processed"
+        return f"{processed_name}.{self.short_name}"
 
     @classmethod
     def from_short_name(cls, short_name: str) -> "Topics":
@@ -39,4 +39,4 @@ class Topics(StrEnum):
     @property
     def short_name(self) -> str:
         """Get the short name of the topic"""
-        return self.value.split("/")[-1]
+        return self.value.split(".")[-1]
