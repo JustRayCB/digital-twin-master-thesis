400ffc6b262713f73d3e4a8b10ad5985039fae0e
JustRayCB <rayancbpro@gmail.com>
2025-04-25 18:12:21 +0200

    Use the second i2c bus for the light sensor

--- bh1750.py
+++ bh1750.py
@@ -1,13 +1,17 @@
 # SPDX-License-Identifier: Unlicense
 import time
-import board
-import adafruit_bh1750
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-print(i2c)
+import adafruit_bh1750
+import board
+import busio
+
+# i2c = board.I2C()  # uses board.SCL and board.SDA
+# Use the second i2c bus
+i2c = busio.I2C(board.D1, board.D0)
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_bh1750.BH1750(i2c)
 
 while True:
     print("%.2f Lux" % sensor.lux)
     time.sleep(1)
+
