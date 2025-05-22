0c806fe6e120364c09c5ad28cb0dbc782b5e2fa6
JustRayCB <rayancbpro@gmail.com>
2025-05-22 16:59:14 +0200

    (code) Initialize all the sensors

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 
+import board
+
 sys.dont_write_bytecode = True
 
 
 from time import sleep
 
-from dt.sensors import MockMoistureSensor, Sensor, SensorManager
+from dt.sensors import (HumiditySensor, LightSensor, MockMoistureSensor,
+                        SensorManager, SoilMoistureSensor, TemperatureSensor)
 from dt.utils import get_logger
 
 
@@ -14,8 +17,25 @@ def main():
     logger.info("Starting main")
 
     sensor_manager = SensorManager()
-    moisture_sensor = MockMoistureSensor("moisture_sensor", 5, 100)
-    sensor_manager.add_sensor(moisture_sensor)
+    # moisture_sensor = MockMoistureSensor("moisture_sensor", 5, 100)
+    # sensor_manager.add_sensor(moisture_sensor)
+
+    """
+    TODO: Moisture Sensor and light sensor use i2c buses, so it needs two pins (SCL and SDA)
+         I need to change how the constructor of the sensors are defined, the pins here for those 
+         sensors are not representative
+         Soil moisture uses the GPIO 0 and 1 pins for SCL and SDA respectively
+         Light sensor uses the GPIO 2 and 3 pins for SCL and SDA respectively
+    """
+    moisturee_sensor = SoilMoistureSensor("moisture_sensor", 5, board.D1)
+    temperature_sensor = TemperatureSensor("temperature_sensor", 5, board.D23)
+    humidity_sensor = HumiditySensor("humidity_sensor", 5, board.D23)
+    light_sensor = LightSensor("light_sensor", 5, board.D3)
+
+    sensor_manager.add_sensor(moisturee_sensor)
+    sensor_manager.add_sensor(temperature_sensor)
+    sensor_manager.add_sensor(humidity_sensor)
+    sensor_manager.add_sensor(light_sensor)
 
     try:
         while True:
