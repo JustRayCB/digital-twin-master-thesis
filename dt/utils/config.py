3d360c24598cf836e85ec8f5119f124c1dae8e4f
JustRayCB <rayancbpro@gmail.com>
2025-10-01 10:33:00 -0400

    (code) Better way of using configuration variable by using dotenv

--- dt/utils/config.py
+++ dt/utils/config.py
@@ -1,21 +1,26 @@
+import os
 from enum import StrEnum
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config(StrEnum):
 
-    KAFKA_URL = "91.86.62.242:9092"  # Kafka messaging service URL
+    KAFKA_URL = os.getenv("KAFKA_URL", "localhost:9092")  # Kafka broker URL
 
     # Database configuration
-    INFLUX_URL = "http://localhost:8086"  # InfluxDB URL
-    INFLUX_TOKEN = (
-        "ttX_TQAsmsu9SAJ0xUGosEdA72b6DJoIFhbOYK8iRzRMHl5dggZAJwbPsGjxt8K-mgdSIPKEkuXUIsGOWxDotA=="
-    )
-    INFLUX_ORG = "dt-ulb"  # InfluxDB organization
-    INFLUX_BUCKET = "plant-health-monitoring"  # InfluxDB bucket
+    INFLUX_URL = os.getenv("INFLUXDB_URL", "http://localhost:8086")  # InfluxDB URL
+    INFLUX_TOKEN = os.getenv("INFLUXDB_TOKEN", "my-influxdb-token")  # InfluxDB token
+    INFLUX_ORG = os.getenv("INFLUXDB_ORG", "dt-ulb")  # InfluxDB organization
+    INFLUX_BUCKET = os.getenv("INFLUXDB_BUCKET", "dt-ulb-bucket")  # InfluxDB bucket
 
     # Flask server URLs
-    FLASK_DASHBOARD_URL = "http://localhost:5000/"
-    FLASK_DB_URL = "http://localhost:5001/"
-    FLASK_AI_URL = "http://localhost:5002/"
+    FLASK_DASHBOARD_URL = os.getenv(
+        "FLASK_DASHBOARD_URL", "http://localhost:5000/"
+    )  # Dashboard URL
+    FLASK_DB_URL = os.getenv("FLASK_DB_URL", "http://localhost:5001/")  # Database service URL
+    FLASK_AI_URL = os.getenv("FLASK_AI_URL", "http://localhost:5002/")  # AI service URL
 
-    MODELS_DIR = "../data/models/"  # Directory for storing models
+    MODELS_DIR = os.getenv("MODELS_DIR", "models/")  # Directory to save/load models
